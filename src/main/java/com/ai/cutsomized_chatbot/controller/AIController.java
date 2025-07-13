66a2c5ec069596f6d260c36884507d77a994e8ba
Thomas Jeberson <thomasjeberson1@gmail.com>
2025-07-13 13:09:04 +0530

    updated CORS netfly

--- src/main/java/com/ai/cutsomized_chatbot/controller/AIController.java
+++ src/main/java/com/ai/cutsomized_chatbot/controller/AIController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "thomaschatbot.netlify.app")
 @RestController
 @RequestMapping("/api/qna")
 public class AIController {

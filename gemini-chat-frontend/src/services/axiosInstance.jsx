9fcb33a4f5b3d2a2e4481dab25cf2a152d32870f
Thomas Jeberson <thomasjeberson1@gmail.com>
2025-07-13 17:00:13 +0530

    Fix: Updated CORS config

--- gemini-chat-frontend/src/services/axiosInstance.jsx
+++ gemini-chat-frontend/src/services/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const credentials=btoa("testuser:test123");
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://thomas-chatbot.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
